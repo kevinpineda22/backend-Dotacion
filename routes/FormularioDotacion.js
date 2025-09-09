79572c7aad56f3347e9182820c7566980bc9b13a
desarrollador software <desarrollo@merkahorrosas.com>
2025-09-09 12:55:26 -0500

    cambios nuevos

--- routes/FormularioDotacion.js
+++ routes/FormularioDotacion.js
@@ -14,4 +14,7 @@ router.get('/dotacion/:documento', obtenerDotacionPorDocumento);
 // Ruta para confirmar entrega de dotación con firma
 router.post('/dotacion/confirmada', confirmarDotacion);
 
+// Ruta para actualizar una dotación por ID
+router.put('/dotaciones/:id', actualizarDotacion); // Actulizar tallas y unidades de la dotación
+
 export default router;  

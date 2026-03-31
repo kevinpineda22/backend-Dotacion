81882cfec7109e785397a9349c076c638060e932
isazaaa <isazamanuel04@gmail.com>
2026-03-31 10:05:57 -0500

    fix: add all HTTP methods to CORS config

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 // Configurar CORS
 app.use(cors({
   origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
